cd5da64904edc5e021a8b0ae637338fbbdcf8ea2
teegoood <teegooodgood@gmail.com>
2025-01-09 18:33:18 +0700

    refactor: progress component

--- src/app/(user)/register/_components/progress/index.tsx
+++ src/app/(user)/register/_components/progress/index.tsx
@@ -10,22 +10,15 @@ interface ProgressProps {
 export default function Progress({ step, className }: ProgressProps) {
   step = Math.min(3, Math.max(1, step));
   return (
-    <div className={cn('flex w-fit', className)}>
-      <div className='-mx-[6px]'>
-        <StepWrapper step={1} isActive={step >= 1} />
-      </div>
-      <div className='-mx-[6px] mt-[18px]'>
-        <Line state={step == 1 ? 'half' : 'full'} />
-      </div>
-      <div className='-mx-[6px]'>
-        <StepWrapper step={2} isActive={step >= 2} />
-      </div>
-      <div className='-mx-[6px] mt-[18px]'>
-        <Line state={step < 2 ? 'empty' : step == 2 ? 'half' : 'full'} />
-      </div>
-      <div className='-mx-[6px]'>
-        <StepWrapper step={3} isActive={step === 3} />
-      </div>
+    <div className={cn('flex w-fit -space-x-2', className)}>
+      <StepWrapper step={1} isActive={step >= 1} />
+      <Line state={step == 1 ? 'half' : 'full'} className="mt-[18px]" />
+      <StepWrapper step={2} isActive={step >= 2} />
+      <Line
+        state={step < 2 ? 'empty' : step == 2 ? 'half' : 'full'}
+        className="mt-[18px]"
+      />
+      <StepWrapper step={3} isActive={step === 3} />
     </div>
   );
 }
