e62b23dfcc07f2c3611063d91300a26e3c7a6bce
teegoood <teegooodgood@gmail.com>
2025-01-09 16:53:08 +0700

    feat: register layout

---
+++ src/app/(user)/register/layout.tsx
@@ -0,0 +1,25 @@
+import { getImageURL } from '@/utils/image';
+import { ChevronLeft } from 'lucide-react';
+import Image from 'next/image';
+
+export default function layout() {
+  return (
+    <div className="min-h-screen w-full">
+      <div className="relative flex aspect-[394/103] items-center justify-between p-4">
+        <Image
+          src={getImageURL('/(user)/register/bg.jpg')}
+          alt="background"
+          fill
+          className="-z-10 brightness-50"
+        />
+        <div>
+          <div className="flex gap-2 font-[400] text-white">
+            <ChevronLeft />
+            กลับ
+          </div>
+          <div className="text-2xl font-[700] text-white">ลงทะเบียน</div>
+        </div>
+      </div>
+    </div>
+  );
+}
