a33d0305239d4ff456d1bc9dbdcc78bc69bde0a9
Soravit Satechaichan <dunk.soravit@gmail.com>
2025-01-12 18:35:54 +0700

    change to component

--- src/app/loading/page.tsx
+++ src/app/loading/page.tsx
@@ -1,20 +1,9 @@
-'use client';
-import Image from 'next/image';
-import { getImageURL } from '@/utils/image';
+import Load from '@/components/loading/loading';
 
 export default function Loading() {
   return (
-    <main className="flex min-h-screen items-center justify-center">
-      <div className="relative">
-        <div className="relative h-32 w-32">
-          <Image src={getImageURL('/loading/Ring.svg')} fill alt="ring" />
-        </div>
-        <div className="absolute inset-0 flex items-center justify-center">
-          <div className="relative h-20 w-14">
-            <Image src={getImageURL('/loading/Logo.svg')} fill alt="logo" />
-          </div>
-        </div>
-      </div>
+    <main>
+      <Load />
     </main>
   );
 }
