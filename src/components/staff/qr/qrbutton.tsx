ddac8960309af6a53bbd2188f4ff5c2609d1b53e
Soravit Satechaichan <dunk.soravit@gmail.com>
2025-01-14 10:16:04 +0700

    First Commit

---
+++ src/components/staff/qr/qrbutton.tsx
@@ -0,0 +1,11 @@
+export default function QRButton() {
+  return (
+    <div className="mt-6 flex justify-center">
+      <img
+        src="/qr-code-placeholder.png"
+        alt="QR Code"
+        className="h-40 w-40 rounded"
+      />
+    </div>
+  );
+}
