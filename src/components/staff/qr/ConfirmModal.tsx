54e71dc0b6e1a3396d65fbf798b79790932163dc
Soravit Satechaichan <dunk.soravit@gmail.com>
2025-01-18 22:11:50 +0700

    Modal variation

--- src/components/staff/qr/ConfirmModal.tsx
+++ src/components/staff/qr/ConfirmModal.tsx
@@ -1,27 +1,31 @@
+import React from 'react';
 import Image from 'next/image';
 import { getImageURL } from '@/utils/image';
 
 interface ConfirmModalProps {
+  modalType: 'confirm' | 'invalid' | 'already';
   userInfo: string | null;
-  closeFn: () => void;
   scanAgain: () => void;
+  closeFn: () => void;
+  time?: string; // Optional prop for 'already' type
 }
 
 const ConfirmModal: React.FC<ConfirmModalProps> = ({
+  modalType,
   userInfo,
-  closeFn,
   scanAgain,
+  closeFn,
+  time,
 }) => {
   const modalClasses =
     'fixed inset-0 z-50 overflow-y-auto justify-center flex bg-gray-500 bg-opacity-75 transition-all ease-in-out duration-300';
 
-  return (
-    <div className={`${modalClasses}`}>
-      <div className={`flex h-full items-center shadow-lg`}>
-        <div className="flex flex-row items-center justify-center">
-          <div className="flex h-full w-72 flex-col items-center justify-center rounded-lg bg-white">
+  const renderContent = () => {
+    switch (modalType) {
+      case 'confirm':
+        return (
+          <>
             <div className="mb-4 mt-6 text-28s font-normal">Confirm!</div>
-
             <Image
               src={getImageURL('/staff/confirm.svg')}
               width={36}
@@ -33,23 +37,80 @@ const ConfirmModal: React.FC<ConfirmModalProps> = ({
               {userInfo}
             </div>
             <div className="my-4 flex flex-col items-center justify-center">
-              {' '}
               <div
                 onClick={() => {
                   scanAgain();
                   closeFn();
                 }}
-                className="flex h-10 w-32 items-center justify-center rounded-lg bg-dark-pink text-white"
+                className="cursor-pointer rounded bg-blue-500 px-4 py-2 text-white"
               >
-                สแกนต่อ
-              </div>{' '}
+                สแกนอีกครั้ง
+              </div>
+            </div>
+          </>
+        );
+      case 'invalid':
+        return (
+          <>
+            <div className="mb-4 mt-6 text-28s font-normal">
+              Invalid QR-Code
+            </div>
+            <Image
+              src={getImageURL('/staff/disable.svg')}
+              width={36}
+              height={36}
+              alt="invalid"
+            />
+            <div className="mt-4 text-lg">สแกนไม่สำเร็จ โปรดลองอีกครั้ง</div>
+            <div className="my-4 flex flex-col items-center justify-center">
               <div
-                onClick={closeFn}
-                className="mt-3 flex h-10 w-32 items-center justify-center rounded-lg bg-dark-pink text-white"
+                onClick={() => {
+                  scanAgain();
+                  closeFn();
+                }}
+                className="cursor-pointer rounded bg-blue-500 px-4 py-2 text-white"
+              >
+                สแกนอีกครั้ง
+              </div>
+            </div>
+          </>
+        );
+      case 'already':
+        return (
+          <>
+            <div className="mb-4 mt-6 text-28s font-normal">Already taken!</div>
+            <Image
+              src={getImageURL('/staff/disable.svg')}
+              width={36}
+              height={36}
+              alt="already"
+            />
+            <div className="mt-4 text-lg">
+              ผู้ใช้สแกน QR-code นี้แล้ว เมื่อเวลา {time}
+            </div>
+            <div className="my-4 flex flex-col items-center justify-center">
+              <div
+                onClick={() => {
+                  closeFn();
+                }}
+                className="cursor-pointer rounded bg-blue-500 px-4 py-2 text-white"
               >
                 กลับ
               </div>
             </div>
+          </>
+        );
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div className={`${modalClasses}`}>
+      <div className={`flex h-full items-center shadow-lg`}>
+        <div className="flex flex-row items-center justify-center">
+          <div className="flex h-full w-72 flex-col items-center justify-center rounded-lg bg-white">
+            {renderContent()}
           </div>
         </div>
       </div>
--- src/components/staff/qr/qrbutton.tsx
+++ src/components/staff/qr/qrbutton.tsx
@@ -1,14 +1,27 @@
 'use client';
 import { useState } from 'react';
-import { getImageURL } from '@/utils/image';
-import Image from 'next/image';
 import { useLiff } from '@/contexts/liff';
 import ConfirmModal from './ConfirmModal';
+import { ScanLine } from 'lucide-react';
+import fs from 'fs';
+import path from 'path';
 // import ErrorModal from './ErrorModal';
 // import AlreadyModal from './AlreadyModal';
 
 export default function QRButton() {
   const { client } = useLiff();
+
+  // Define the file path
+  const filePath = path.join(__dirname, 'qrCodeValue.json');
+
+  // Read the existing file content
+  let existingValues: string[] = [];
+  if (fs.existsSync(filePath)) {
+    const data = fs.readFileSync(filePath, 'utf8');
+    const fileContent = JSON.parse(data);
+    existingValues = fileContent.values || [];
+  }
+
   const [qrCodeValue, setQrCodeValue] = useState<string | null>(null);
   const [modalType, setModalType] = useState<
     'confirm' | 'error' | 'already' | null
@@ -28,6 +41,18 @@ export default function QRButton() {
         setQrCodeValue(value);
         setIsModalOpen(true);
         setModalType('confirm');
+
+        // Check if the value already exists
+        if (!existingValues.includes(value)) {
+          // Add the new value
+          existingValues.push(value);
+
+          // Write the updated content back to the file
+          fs.writeFileSync(
+            filePath,
+            JSON.stringify({ values: existingValues }, null, 2),
+          );
+        }
       } else if (value === 'taken') {
         setModalType('already');
         setIsModalOpen(true);
@@ -51,12 +76,7 @@ export default function QRButton() {
           onClick={openQRScanner}
           className="flex flex-row justify-center gap-2"
         >
-          <Image
-            src={getImageURL('/staff/qr.png')}
-            width={24}
-            height={24}
-            alt="scan"
-          />
+          <ScanLine size={26} className="mt-1" />
           <p className="mt-1">คลิกเพื่อสแกน</p>
         </div>
       </div>
@@ -65,6 +85,7 @@ export default function QRButton() {
           userInfo={qrCodeValue}
           scanAgain={openQRScanner}
           closeFn={() => setIsModalOpen(false)}
+          modalType={modalType}
         />
       )}
       {/* {modalType === 'error' && <ErrorModal onClose={closeModal} />}
