55bf74942c89dab816f4955946871108ff18b720
TeeGooodGood <122004889+TeeGoood@users.noreply.github.com>
2025-01-23 21:41:11 +0700

    deploy: set up docker

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone',
   compiler: {
     // Remove console logs only in production, excluding error logs
     removeConsole:
