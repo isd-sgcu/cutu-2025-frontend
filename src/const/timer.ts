da0fa3063f54770823e5816647b5a6681a190b85
TeeGooodGood <122004889+TeeGoood@users.noreply.github.com>
2025-01-23 22:06:37 +0700

    fix: stupid safari date parsing

--- src/const/timer.ts
+++ src/const/timer.ts
@@ -9,7 +9,8 @@ if (!rawTargetDate) {
   throw new Error('Environment variable NEXT_PUBLIC_TARGET_DATE is missing.');
 }
 
-const parsedDate = new Date(rawTargetDate);
+const t = rawTargetDate.split(/[^0-9]/).map(t => parseInt(t));
+const parsedDate = new Date(t[0], t[1] - 1, t[2]);
 if (isNaN(parsedDate.getTime())) {
   throw new Error(
     `Invalid date format in NEXT_PUBLIC_TARGET_DATE: ${rawTargetDate}`,
