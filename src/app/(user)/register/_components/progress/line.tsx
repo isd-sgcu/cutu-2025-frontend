d9c7a35f9420365a4d50e7f62267d11159d3d5ee
TeeGooodGood <122004889+TeeGoood@users.noreply.github.com>
2025-01-25 21:52:18 +0700

    style: progress gradient

--- src/app/(user)/register/_components/progress/line.tsx
+++ src/app/(user)/register/_components/progress/line.tsx
@@ -3,20 +3,27 @@ import { cn } from '@/lib/utils';
 interface LineProps {
   state: 'empty' | 'half' | 'full';
   className?: string;
+  startColor: string;
+  stopColor: string;
 }
 
-export default function Line({ state, className }: LineProps) {
-  return (
-    <div
-      className={cn(
-        'h-[3px] min-w-10',
-        {
-          'bg-light-gray': state == 'empty',
-          'bg-gradient-to-r from-dark-pink to-light-gray': state == 'half',
-          'bg-dark-pink': state == 'full',
-        },
-        className,
-      )}
-    ></div>
-  );
+export default function Line({
+  state,
+  className,
+  stopColor,
+  startColor,
+}: LineProps) {
+  let bg: string;
+  switch (state) {
+    case 'empty':
+      bg = 'bg-light-gray';
+      break;
+    case 'half':
+      bg = 'bg-gradient-to-r to-light-gray ' + startColor;
+      break;
+    default:
+      bg = 'bg-gradient-to-r ' + startColor + ' ' + stopColor;
+  }
+
+  return <div className={cn('h-[3px] min-w-10', bg, className)}></div>;
 }
