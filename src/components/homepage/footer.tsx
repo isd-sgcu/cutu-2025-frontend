8a8623111eb4aa99197a2e61152e239dbe28770d
TeeGooodGood <122004889+TeeGoood@users.noreply.github.com>
2025-01-25 21:24:30 +0700

    chore: add social link

--- src/components/homepage/footer.tsx
+++ src/components/homepage/footer.tsx
@@ -1,19 +1,20 @@
 import { getImageURL } from '@/utils/image';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Footer() {
   return (
     <section className="bg-black p-2">
       <div className="flex flex-row items-center justify-center gap-2 text-sm">
         <p className="text-white">Powered by</p>
-        <div className="flex">
+        <Link className="flex" href={'https://www.instagram.com/isd.sgcu/'}>
           <Image
             alt="isdlogo"
             src={getImageURL('/homepage/isd.svg')}
             width={24}
             height={15}
           />
-        </div>
+        </Link>
         <div className="flex">
           <Image
             alt="sgculogo"
