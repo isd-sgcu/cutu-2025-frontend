aec9870c07cb155f2fe68ae6bfb6b4fdfaf71ada
TeeGooodGood <122004889+TeeGoood@users.noreply.github.com>
2025-01-26 21:04:08 +0700

    chore: disable react strict mode

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: false,
   output: 'standalone',
   compiler: {
     // Remove console logs only in production, excluding error logs
