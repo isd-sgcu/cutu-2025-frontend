5b9b6942aea24b2766f4a70f3b75bf1af9f15cf1
TeeGooodGood <122004889+TeeGoood@users.noreply.github.com>
2025-01-28 03:34:43 +0700

    feat: gateway

---
+++ src/app/gateway/page.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import Load from '@/components/loading/loading';
+import { useAuth } from '@/contexts/auth';
+import { useSearchParams } from 'next/navigation';
+import React from 'react';
+
+const pages = {
+  member: {
+    home: '/',
+    qr: '/qr',
+    profile: '/edit',
+  },
+  staff: {
+    home: '/',
+    qr: '/staff/qr',
+    profile: '/edit',
+  },
+  admin: {
+    home: '/',
+    qr: '/staff/qr',
+    profile: '/admin/dashboard',
+  },
+};
+
+export default function Page() {
+  const { user, isInitialized } = useAuth();
+  const searchParams = useSearchParams();
+  const page = searchParams.get('page') || 'home';
+
+  if (!isInitialized) {
+    return <Loading />;
+  }
+
+  window.location.href =
+    pages[user?.role || 'member'][page as 'home' | 'qr' | 'profile'];
+  return <Loading />;
+}
+
+function Loading() {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center">
+      <Load />
+    </div>
+  );
+}
