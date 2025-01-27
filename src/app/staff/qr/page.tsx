66cde42db3d35ad21caf785542009a28f88f85ec
TeeGooodGood <122004889+TeeGoood@users.noreply.github.com>
2025-01-28 02:56:39 +0700

    feat: protect route

--- src/app/staff/qr/page.tsx
+++ src/app/staff/qr/page.tsx
@@ -2,15 +2,18 @@ import Background from '@/components/staff/qr/background';
 import TopPart from '@/components/staff/qr/toppart';
 import Edit from '@/components/staff/qr/editbutton';
 import QrButton from '@/components/staff/qr/qrbutton';
+import Protect from '@/components/protect';
 export default function Home() {
   return (
     <main className="relative min-h-screen">
-      <Background />
-      <div className="relative flex flex-col items-center pt-32">
-        <TopPart />
-        <QrButton />
-        <Edit />
-      </div>
+      <Protect roles={['staff', 'admin']}>
+        <Background />
+        <div className="relative flex flex-col items-center pt-32">
+          <TopPart />
+          <QrButton />
+          <Edit />
+        </div>
+      </Protect>
     </main>
   );
 }
