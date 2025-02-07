f326edad4af51d4dcaa8e1c3e06443098d62f552
TeeGooodGood <122004889+TeeGoood@users.noreply.github.com>
2025-02-07 17:50:26 +0700

    change: limit role access toast message

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -2,5 +2,9 @@ import Protect from '@/components/protect';
 import React from 'react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <Protect roles={['admin']}>{children}</Protect>;
+  return (
+    <Protect roles={['admin']} callBack="คุณไม่มีสิทธิเข้าหน้านี้">
+      {children}
+    </Protect>
+  );
 }
--- src/app/staff/layout.tsx
+++ src/app/staff/layout.tsx
@@ -2,5 +2,9 @@ import Protect from '@/components/protect';
 import React from 'react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <Protect roles={['admin', 'staff']}>{children}</Protect>;
+  return (
+    <Protect roles={['admin', 'staff']} callBack="คุณไม่มีสิทธิเข้าหน้านี้">
+      {children}
+    </Protect>
+  );
 }
